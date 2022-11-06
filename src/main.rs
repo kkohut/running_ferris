25cef097783147c81334bc0a93a25b48fff8c7d9
Kevin Kohut <kevin.kohut@student.fhws.de>
2022-11-06 18:49:31 +0100

    Reformat

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate reqwest;
 
-use std::time::{Instant, Duration};
 use colored::Colorize;
+use std::time::{Duration, Instant};
 
 const FILE_SIZE_IN_MB: i32 = 100;
 
@@ -11,14 +11,19 @@ fn main() {
     let duration = start_time.elapsed();
     println!("Download finished in {duration:.3?}");
     let speed = calculate_speed(FILE_SIZE_IN_MB, duration);
-    println!("Download speed: {}", format!("{speed:.3}Mb/s").green().bold());
+    println!(
+        "Download speed: {}",
+        format!("{speed:.3}Mb/s").green().bold()
+    );
 }
 
 fn perform_download(file_size: i32) {
     const BASE_URL: &str = "http://speedtest.tele2.net";
     let url = format!("{BASE_URL}/{file_size}MB.zip");
     println!("Downloading {FILE_SIZE_IN_MB}MB from {url}...");
-    let _ = reqwest::blocking::get(url).unwrap().bytes();
+    let _ = reqwest::blocking::get(url)
+        .expect("Error while downloading.")
+        .bytes();
 }
 
 fn calculate_speed(file_size_in_mb: i32, duration: Duration) -> f64 {
