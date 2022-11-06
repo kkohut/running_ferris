7d796f181781051cb7bb95a9c700006f53c9091d
Kevin Kohut <kevin.kohut@student.fhws.de>
2022-11-06 21:39:46 +0100

    Add file size choice via user input

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,62 @@
-extern crate reqwest;
+// extern crate reqwest;
 
-use colored::Colorize;
 use std::time::{Duration, Instant};
 
-const FILE_SIZE_IN_MB: i32 = 100;
+use colored::Colorize;
+use text_io::read;
 
 fn main() {
+    let chosen_file_size = ask_for_file_size();
     let start_time = Instant::now();
-    perform_download(FILE_SIZE_IN_MB);
+    perform_download(chosen_file_size);
     let duration = start_time.elapsed();
     println!("Download finished in {duration:.3?}");
-    let speed = calculate_speed(FILE_SIZE_IN_MB, duration);
+    let speed = calculate_speed(chosen_file_size, duration);
     println!(
         "Download speed: {}",
         format!("{speed:.3}Mb/s").green().bold()
     );
 }
 
-fn perform_download(file_size: i32) {
+fn ask_for_file_size() -> usize {
+    let allowed_file_sizes_in_mb: [usize; 6] = [10, 20, 50, 100, 500, 1000];
+    print_input_instructions(allowed_file_sizes_in_mb);
+    let mut optional_file_size_in_mb: Option<usize> = None;
+    while optional_file_size_in_mb.is_none() {
+        let entered_number: usize = read!();
+        if (entered_number - 1) > allowed_file_sizes_in_mb.len() {
+            print_input_instructions(allowed_file_sizes_in_mb);
+        } else {
+            optional_file_size_in_mb = Some(allowed_file_sizes_in_mb[entered_number - 1]);
+        }
+    }
+    optional_file_size_in_mb.expect("Error occured while entering file size.")
+}
+
+fn print_input_instructions(allowed_file_sizes_in_mb: [usize; 6]) {
+    println!("Choose your preferred filesize by entering the corresponding number in the square brackets. \
+    Larger files result in preciser results.");
+    for (index, size) in allowed_file_sizes_in_mb.iter().enumerate() {
+        println!("[{}] {}MB", index + 1, size);
+    }
+}
+
+fn perform_download(file_size_in_mb: usize) {
     const BASE_URL: &str = "http://speedtest.tele2.net";
-    let url = format!("{BASE_URL}/{file_size}MB.zip");
-    println!("Downloading {FILE_SIZE_IN_MB}MB from {url}...");
+    let path = if file_size_in_mb >= 1000 {
+        let file_size_in_gb = file_size_in_mb / 1000;
+        format!("{file_size_in_gb}GB.zip")
+    } else {
+        format!("{file_size_in_mb}MB.zip")
+    };
+    let url = format!("{BASE_URL}/{path}");
+    println!("Downloading {file_size_in_mb}MB from {url}...");
     let _ = reqwest::blocking::get(url)
         .expect("Error while downloading.")
         .bytes();
 }
 
-fn calculate_speed(file_size_in_mb: i32, duration: Duration) -> f64 {
+fn calculate_speed(file_size_in_mb: usize, duration: Duration) -> f64 {
     const BITS_PER_BYTE: i32 = 8;
-    (file_size_in_mb * BITS_PER_BYTE) as f64 / duration.as_secs() as f64
+    (file_size_in_mb as i32 * BITS_PER_BYTE) as f64 / duration.as_secs() as f64
 }
